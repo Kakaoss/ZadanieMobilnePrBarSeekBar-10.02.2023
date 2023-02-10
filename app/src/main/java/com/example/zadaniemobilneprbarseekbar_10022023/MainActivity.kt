c570dd9fbecaf7db6386d92621464e8329631af7
mihau11 <michi.leszczynski@gmaail.com>
2023-02-10 14:53:35 +0100

    ustawienie domyslnych szerokowsci oraz wysokosci obrazow przy wlaczeniu programu

--- app/src/main/java/com/example/zadaniemobilneprbarseekbar_10022023/MainActivity.kt
+++ app/src/main/java/com/example/zadaniemobilneprbarseekbar_10022023/MainActivity.kt
@@ -13,18 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val obraz1 = findViewById<ImageView>(R.id.obraz1)
+        val obraz2 = findViewById<ImageView>(R.id.obraz2)
+        val obraz3 = findViewById<ImageView>(R.id.obraz3)
+        val Button_reset = findViewById<Button>(R.id.btnReset)
+        val seekbarpion1 = findViewById<SeekBar>(R.id.seekbarPion1)
+        val seekbarpion2 = findViewById<SeekBar>(R.id.seekBarPion2)
+        val seekbarpion3 = findViewById<SeekBar>(R.id.seekBarPion3)
+        val seekbarPoziom1 = findViewById<SeekBar>(R.id.seekBarPoziom1)
+        val seekbarPoziom2 = findViewById<SeekBar>(R.id.seekBarPoziom2)
+        val seekbarPoziom3 = findViewById<SeekBar>(R.id.seekBarPoziom3)
+
+
+        obraz1.layoutParams.height = StandardowaWysokosc
+        obraz1.layoutParams.width = StandardowaSzerokosc
+        obraz2.layoutParams.height = StandardowaWysokosc
+        obraz2.layoutParams.width = StandardowaSzerokosc
+        obraz3.layoutParams.height = StandardowaWysokosc
+        obraz3.layoutParams.width = StandardowaSzerokosc
+
+
     }
-
-    val obraz1 = findViewById<ImageView>(R.id.obraz1)
-    val obraz2 = findViewById<ImageView>(R.id.obraz2)
-    val obraz3 = findViewById<ImageView>(R.id.obraz3)
-    val Button_reset = findViewById<Button>(R.id.btnReset)
-    val seekbarpion1 = findViewById<SeekBar>(R.id.seekbarPion1)
-    val seekbarpion2 = findViewById<SeekBar>(R.id.seekBarPion2)
-    val seekbarpion3 = findViewById<SeekBar>(R.id.seekBarPion3)
-    val seekbarPoziom1 = findViewById<SeekBar>(R.id.seekBarPoziom1)
-    val seekbarPoziom2 = findViewById<SeekBar>(R.id.seekBarPoziom2)
-    val seekbarPoziom3 = findViewById<SeekBar>(R.id.seekBarPoziom3)
-
-
 }
